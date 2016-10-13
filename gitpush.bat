64fa06502f2f23cd38d55124308fa145485cbff8
chalktalkimages <equitydma@scglobal.ad.scotiacapital.com>
2016-10-13 11:49:07 -0400

    imagescommit

---
+++ gitpush.bat
@@ -0,0 +1,5 @@
+pushd C:\incoming\ChalkServer\Chalkimages
+git config remote.origin.url https://chalktalkimages:abcd123@github.com/chalktalkimages/Chalkimages.git
+git add -A
+git commit -am imagescommit
+git push
