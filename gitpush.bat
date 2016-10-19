5e417d6dd3f4e7c4111c97e3f84d8f179c5e9cda
Bill Liu <liujidon@gmail.com>
2016-10-19 10:44:23 -0400

    imagescommit

--- gitpush.bat
+++ gitpush.bat
@@ -1,4 +1,4 @@
-pushd C:\incoming\ChalkServer\Chalkimages
+pushd \\t65-w7-eqcash\incoming\ChalkServer\Chalkimages\
 git config remote.origin.url https://chalktalkimages:abcd123@github.com/chalktalkimages/Chalkimages.git
 git add -A
 git commit -am imagescommit
