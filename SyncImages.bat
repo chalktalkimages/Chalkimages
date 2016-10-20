8db7bf935b58ed3b25fefc04b18cbec28bc96f7a
chalktalkimages <equitydma@scglobal.ad.scotiacapital.com>
2016-10-20 12:48:51 -0400

    imagescommit

--- SyncImages.bat
+++ SyncImages.bat
@@ -1,1 +1,1 @@
-WMIC /user:equitydma /password:123456Ad /node:"t65-w7-eqcash" process call create "cmd /c C:\incoming\ChalkServer\gitpush.bat"
+wmic /user:equitydma /password:123456Ad /node:"t65-w7-eqcash" process call create "cmd /c C:\incoming\ChalkServer\gitpush.bat"
