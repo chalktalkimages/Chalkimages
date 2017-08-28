4dcad5da8159937cc37266fded30dd67704d9eae
chalktalkimages <equitydma@scglobal.ad.scotiacapital.com>
2017-08-28 10:29:48 -0400

    imagescommit

---
+++ RUNclearOldChalkImagesArchive.bat
@@ -0,0 +1,3 @@
+wmic /user:equitydma /password:123456Ad /node:"t65-w7-eqcash" process call create "cmd /c C:\incoming\ChalkServer\clearOldChalkImagesArchive.bat"
+wmic /user:equitydma /password:123456Ad /node:"t65-w7-eqcash" process call create "cmd /c C:\incoming\ChalkServer\gitpush.bat"
+pause
